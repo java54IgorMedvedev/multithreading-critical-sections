6be528f1adee459df3ed1349c4d74295bd20ee77
IgorMedvedev <psydov81@gmail.com>
2024-08-06 19:03:32 +0300

    hw #43

---
+++ src/telran/multithreading/CounterUpdater.java
@@ -0,0 +1,29 @@
+package telran.multithreading;
+
+public class CounterUpdater extends Thread {
+	private static volatile long counter; 
+	private static final Object mutex = new Object();
+	private int nRuns;
+
+	public CounterUpdater(int nRuns) {
+		this.nRuns = nRuns;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < nRuns; i++) {
+			counterIncrement();
+		}
+	}
+
+	synchronized static private void counterIncrement() {
+		counter++;
+
+	}
+
+	public static long getCounter() {
+
+		return counter;
+	}
+
+}
